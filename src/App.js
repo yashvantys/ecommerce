be23c2f165fca9fdfff3765f16246fbc2906b358
yashvantys <yashvanty@gmail.com>
2020-12-13 13:25:23 +0530

    action type added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import {connect} from 'react-redux';
 
 import './App.css';
@@ -47,15 +47,19 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to="" />):(<SignInAndSignUpPage/>)} />
         </Switch>
       </div>
     );
   }
-};
+}
+
+const mapStateToProps = ({user}) => ({
+  currentUser:user.currentUser
+})
 
 const mapDistatchToProps = dispatch => ({
   setCurrentuser: user => dispatch(setCurrentuser(user))
 })
 
-export default connect(null, mapDistatchToProps)(App);
+export default connect(mapStateToProps, mapDistatchToProps)(App);
