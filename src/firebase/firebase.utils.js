cef2285bd30afcd2ebe5db02bffc2a04f378a069
yashvantys <yashvanty@gmail.com>
2020-12-13 07:13:04 +0530

    login and signup completed

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,40 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {    
-    apiKey: "AIzaSyC3-BibDZjg__vplKDBJBAI3JIMz9aOd-s",
-    authDomain: "ecommerce-16c70.firebaseapp.com",
-    projectId: "ecommerce-16c70",
-    storageBucket: "ecommerce-16c70.appspot.com",
-    messagingSenderId: "977619731959",
-    appId: "1:977619731959:web:a6b0625bedca7259d1daf2",
-    measurementId: "G-PFRD5F5K1P"
+    apiKey: "AIzaSyC5hjz22-pBiCeCejtVIg8AJ3NHyfCqylI",
+    authDomain: "react-ecommerce-d0a2b.firebaseapp.com",
+    databaseURL: "https://react-ecommerce-d0a2b-default-rtdb.firebaseio.com",
+    projectId: "react-ecommerce-d0a2b",
+    storageBucket: "react-ecommerce-d0a2b.appspot.com",
+    messagingSenderId: "713771205866",
+    appId: "1:713771205866:web:72006cf2ea7bdfecd34393",
+    measurementId: "G-ZZDF82RCHL"
     };
+
+    export const createUserProfileDocument = async (userAuth, additionalData) => {
+        if(!userAuth) return;
+
+        const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+        const snapShot = await userRef.get();
+
+        if (!snapShot.exists) {
+            const { displayName, email } = userAuth;
+            const createdAt = new Date();
+            try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+            } catch (error) {
+            console.log('error creating user', error.message);
+            }
+        }
+
+        return userRef;
+    }
 
 firebase.initializeApp(config);
 
