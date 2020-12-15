924ab84cd8d9c61a3de97c1089e4d36d18abdcba
yashvantys <yashvanty@gmail.com>
2020-12-15 20:44:26 +0530

    cart remove and add functionality added

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,7 @@
 import CartActionTypes from './cart.types';
 
-import {addItemToCart} from './cart.utils'
+import {addItemToCart, removeItemFromCart} from './cart.utils'
+import CartItem from '../../components/cart-item/cart-item.component';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -19,6 +20,19 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 cartItems:addItemToCart(state.cartItems, action.payload)
             }
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems:state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+            }
+
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems:removeItemFromCart(state.cartItems, action.payload)
+
+            }
+            
 
         default:
             return state;
